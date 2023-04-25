8d6e3519abe308c9d504e667f7c8d9c6f7cdba55
Vardhan Dintakurthi <vardhan.performs@gmail.com>
2023-04-24 23:35:41 -0500

    initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xc36ca0DCE6294E5358e83d794eb7139B607EAaB3";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
